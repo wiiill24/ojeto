6b2834e3049a4da555f5375849dfc82e93b82822
wiiill24 <139027544+wiiill24@users.noreply.github.com>
2025-12-16 16:53:56 -0400

    Delete unibot_test directory

--- unibot_test/app/views/layouts/footer.php
+++
@@ -1,8 +0,0 @@
-    <?php if (isset($js)): ?>
-        <?php foreach ($js as $script): ?>
-            <script src="<?php echo $script; ?>"></script>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</body>
-</html>
-
